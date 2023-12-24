b140b8f3890e086cf2b5927a7c29b649a9b3c7b2
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-12-24 21:13:44 +0200

    Api

--- server.js
+++ server.js
@@ -24,48 +24,73 @@ app.post('/payment', async (req, res) => {
     )
 
     const tokenResponseData = await tokenResponse.json()
-
-    console.log(tokenResponseData)
     
-    // const authToken = tokenResponseData.token
-
-    // console.log(authToken)
+    const authToken = tokenResponseData.token
     
-    // const { name, description, amount_cents, quantity } = req.body
+    const { name, description, amount_cents, quantity, email, phone_number, first_name, last_name } = req.body
     
-    // const orderResponse = await fetch('https://accept.paymob.com/api/ecommerce/orders',{
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //         "auth_token": `${authToken}`,
-    //         "delivery_needed": "false",
-    //         "amount_cents": amount_cents.toString(),
-    //         "currency": "EGP",
-    //         "Items": [
-    //             {
-    //                 "name": name,
-    //                 "description": description,
-    //                 "amount_cents": amount_cents.toString(),
-    //                 "quantity": quantity.toString()
-    //             }
-    //         ],
+    const orderResponse = await fetch('https://accept.paymob.com/api/ecommerce/orders',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "auth_token": `${authToken}`,
+            "delivery_needed": "false",
+            "amount_cents": amount_cents.toString(),
+            "currency": "EGP",
+            "Items": [
+                {
+                    "name": name,
+                    "description": description,
+                    "amount_cents": amount_cents.toString(),
+                    "quantity": quantity.toString()
+                }
+            ],
 
-    //     })
-    // })
+        })
+    })
 
-    // const orderResponseData = await orderResponse.json()
-
-    // console.log(orderResponseData)
+    const orderResponseData = await orderResponse.json()
     
-    // const orderId = orderResponseData.id
+    const orderId = orderResponseData.id
 
-    // console.log(orderId)
+    const paymentTokenRequest = await fetch('https://accept.paymob.com/api/acceptance/payment_keys', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "auth_token": authToken,
+            "amount_cents": amount_cents.toString(),
+            "expiration": 3600,
+            "order_id": orderId.toString(),
+            "billing_data": {
+                "email": email,
+                "first_name": first_name,
+                "last_name": last_name,
+                "phone_number": phone_number,
+                "country": "EG",
+                "city": "cairo",
+                "postal_code": "11632",
+                "shipping_method": "NA",
+                "street": "NA", 
+                "building": "NA",
+                "floor": "NA",
+                "apartment": "NA",
+                "state": "NA"
+            },
+            "currency": "EGP",
+            "integration_id": 4422174,
+            "lock_order_when_paid": "false"
+        })
+    })
 
-    // return res.status(201).json('a7a')
+    const paymentTokenData = await paymentTokenRequest.json()
+
+    return res.status(201).json({link: `https://accept.paymob.com/api/acceptance/iframes/810705?payment_token=${paymentTokenData.token}`})
 })
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
-  });
+});
